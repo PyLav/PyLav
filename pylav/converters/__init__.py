b68638dc9dd85eecb4abfa3de440fcd962133c09
Draper <27962761+Drapersniper@users.noreply.github.com>
2022-08-01 17:53:31 +0100

    fuck the range converter

--- pylav/converters/__init__.py
+++ pylav/converters/__init__.py
@@ -4,4 +4,3 @@ from pylav.converters.nodes import NodeConverter as TrackConverter
 from pylav.converters.playlist import PlaylistConverter as PlaylistConverter
 from pylav.converters.query import QueryConverter as QueryConverter
 from pylav.converters.query import QueryPlaylistConverter as QueryPlaylistConverter
-from pylav.converters.ranges import RangeConverter as RangeConverter
