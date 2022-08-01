2f4166315a2f15e19055600c5d9f2faabb394818
Draper <27962761+Drapersniper@users.noreply.github.com>
2022-08-01 19:32:03 +0100

    add useSeekGhosting, resamplingQuality and opusEncodingQuality to the application.yaml

--- pylav/sql/clients/updater.py
+++ pylav/sql/clients/updater.py
@@ -18,13 +18,21 @@ class UpdateSchemaManager:
 
         # FIXME: This should be whatever value the first release is or alternatively `self._client.lib_version`
 
-        if (await self._client.lib_db_manager.get_bot_db_version()).version == parse_version("0.0.0.0"):
+        if (await self._client.lib_db_manager.get_bot_db_version()).version <= parse_version("0.0.0.0"):
             await self._client.lib_db_manager.update_bot_dv_version("0.0.0.1")
 
-        if (await self._client.lib_db_manager.get_bot_db_version()).version == parse_version("0.0.0.1"):
+        if (await self._client.lib_db_manager.get_bot_db_version()).version <= parse_version("0.0.0.1"):
             await self._client.lib_db_manager.update_bot_dv_version("0.0.0.2")
             full_data = await self._client.node_db_manager.get_bundled_node_config()
             full_data.yaml["lavalink"]["server"]["trackStuckThresholdMs"] = 10000
+            await full_data.save()
+
+        if (await self._client.lib_db_manager.get_bot_db_version()).version <= parse_version("0.3.1"):
+            await self._client.lib_db_manager.update_bot_dv_version("0.3.2")
+            full_data = await self._client.node_db_manager.get_bundled_node_config()
+            full_data.yaml["lavalink"]["server"]["opusEncodingQuality"] = 10
+            full_data.yaml["lavalink"]["server"]["resamplingQuality"] = "LOW"
+            full_data.yaml["lavalink"]["server"]["useSeekGhosting"] = True
             await full_data.save()
 
         await self._client.lib_db_manager.update_bot_dv_version(__VERSION__)
