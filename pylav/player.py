3d4e1b94fb5e454028679ae8912d57ef41e22520
Draper <27962761+Drapersniper@users.noreply.github.com>
2022-09-02 16:11:18 +0100

    fix AttributeError: 'NoneType' object has no attribute 'stream'

--- pylav/player.py
+++ pylav/player.py
@@ -1747,7 +1747,7 @@ class Player(VoiceProtocol):
         if previous_track_description:
             val = f"{previous_track_description}\n"
             val += "{translation}: `{duration}`\n".format(
-                duration=_("LIVE") if self.next_track.stream else format_time(self.next_track.duration),
+                duration=_("LIVE") if self.last_track.stream else format_time(self.last_track.duration),
                 translation=discord.utils.escape_markdown(_("Duration")),
             )
             if rq := self.last_track.requester:
