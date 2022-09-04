e2b8b6fc308b9771aa57328731afaaf7c59c7d7a
Draper <27962761+Drapersniper@users.noreply.github.com>
2022-09-04 14:12:54 +0100

    add typehints

--- pylav/migrations/__init__.py
+++ pylav/migrations/__init__.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
 
+from piccolo.engine import PostgresEngine, SQLiteEngine
 
-def run_low_level_migrations(db) -> None:
+
+def run_low_level_migrations(db: PostgresEngine | SQLiteEngine) -> None:
     """
     Runs migrations.
     """
