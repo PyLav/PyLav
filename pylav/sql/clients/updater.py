6aba459137dfc92d0ad35a6c37ba9add2e6440bb
draper <27962761+Drapersniper@users.noreply.github.com>
2022-10-09 18:38:20 +0100

    Fix Import order

--- pylav/sql/clients/updater.py
+++ pylav/sql/clients/updater.py
@@ -7,7 +7,7 @@ from packaging.version import parse as parse_version
 
 from pylav.constants import BUNDLED_NODES_IDS
 from pylav.exceptions import EntryNotFoundError
-from pylav.managed_node import LAVALINK_DOWNLOAD_DIR
+from pylav.utils.built_in_node import NODE_DEFAULT_SETTINGS
 
 if TYPE_CHECKING:
     from pylav.client import Client
@@ -22,7 +22,7 @@ class UpdateSchemaManager:
     async def run_updates(self):
         """Run through schema migrations"""
         from pylav import __VERSION__
-        from pylav.utils.built_in_node import NODE_DEFAULT_SETTINGS
+        from pylav.managed_node import LAVALINK_DOWNLOAD_DIR
 
         current_version = await self._client.lib_db_manager.get_bot_db_version().fetch_version()
         if current_version == parse_version("0.0.0.0.9999"):
