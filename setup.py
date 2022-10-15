fc7091fc15ec2b0b1e035736d0ca2d44c61f88ec
draper <27962761+Drapersniper@users.noreply.github.com>
2022-10-15 16:47:07 +0100

    Fix the build - Thankkkks Jack
    Signed-off-by: draper <27962761+Drapersniper@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
+import re
+
 from setuptools import setup
 
-from pylav import __VERSION__
+with open("./pylav/__init__.py") as f:
+    version = re.search(r'^__version__\s*=\s*__VERSION__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE)[1]
 
 setup(
-    version=__VERSION__,
+    version=version,
 )
