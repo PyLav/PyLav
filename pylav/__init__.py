d844cc28b65ae89b128929d7452097383661f38c
Draper <27962761+Drapersniper@users.noreply.github.com>
2022-10-16 15:40:58 +0000

    [skip ci]Automated version bump: v0.10.5.1 >> v0.11.0.0

--- pylav/__init__.py
+++ pylav/__init__.py
@@ -1,5 +1,5 @@
 __ALL__ = ("__VERSION__", "__version__")
-__version__ = __VERSION__ = "0.10.5.1"
+__version__ = __VERSION__ = "0.11.0.0"
 __ZERO__ = b"MHp2Zg=="
 __MOMONGA__ = b"NTh3Yw=="
 __LIGHT__ = b"|OW5hMQ=="
