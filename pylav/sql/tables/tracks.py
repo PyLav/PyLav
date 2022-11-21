e51ec16489283331ff8b1208593d12ec9d0d71eb
Draper <27962761+Drapersniper@users.noreply.github.com>
2022-11-21 23:47:29 +0000

    stop using transaction as they get cancelled if a new one is started causing writes to only partially happen
    Signed-off-by: Draper <27962761+Drapersniper@users.noreply.github.com>

--- pylav/sql/tables/tracks.py
+++ pylav/sql/tables/tracks.py
@@ -1,9 +1,11 @@
 from __future__ import annotations
 
+from asyncpg import UniqueViolationError
 from piccolo.columns import M2M, Boolean, Integer, LazyTableReference, Text
 from piccolo.table import Table
 
 from pylav.sql.tables.init import DB
+from pylav.utils import _LOCK, LOGGER, _synchronized
 
 
 class TrackRow(Table, db=DB, tablename="track"):
@@ -19,3 +21,16 @@ class TrackRow(Table, db=DB, tablename="track"):
     encoded = Text(null=False, index=True, primary_key=True)
     queries = M2M(LazyTableReference("TrackToQueries", module_path="pylav.sql.tables.m2m"))
     playlists = M2M(LazyTableReference("TrackToPlaylists", module_path="pylav.sql.tables.m2m"))
+
+    @classmethod
+    @_synchronized(_LOCK)
+    async def get_or_create(cls, encoded: str, kwargs: dict):
+        try:
+            return await cls.objects().get_or_create(cls.encoded == encoded, kwargs)
+        except UniqueViolationError:
+            obj = cls(encoded=encoded, **kwargs)
+            obj._exists_in_db = True
+            return obj
+        except Exception as e:
+            LOGGER.trace(f"Error while creating track: %s", e, exc_info=True)
+            raise e
