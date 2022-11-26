f3af0631150a0711cec399a29056022f2a3fec27
Draper <27962761+Drapersniper@users.noreply.github.com>
2022-11-26 12:52:58 +0000

    bring up to date and few fixes
    Signed-off-by: Draper <27962761+Drapersniper@users.noreply.github.com>

--- pylav/config_migrations/__init__.py
+++ pylav/config_migrations/__init__.py
@@ -2,4 +2,4 @@ from pylav._logging import getLogger
 
 LOGGER = getLogger("PyLav.migrator")
 
-__ALL__ = ("LOGGER",)
+__all__ = ("LOGGER",)
--- pylav/sql/__init__.py
+++ pylav/sql/__init__.py
@@ -2,4 +2,4 @@ from __future__ import annotations
 
 from pylav.sql import aiohttp_postgres_cache, caching, clients, models, tables
 
-__ALL__ = ("aiohttp_postgres_cache", "caching", "clients", "models", "tables")
+__all__ = ("aiohttp_postgres_cache", "caching", "clients", "models", "tables")
