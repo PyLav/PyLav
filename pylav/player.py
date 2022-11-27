011166d406d5d18048ac95fb8a01177d29663b88
Draper <27962761+Drapersniper@users.noreply.github.com>
2022-11-27 22:08:21 +0000

    Set Pitch and Rate to 20% gain on nightcore preset
    Signed-off-by: Draper <27962761+Drapersniper@users.noreply.github.com>

--- pylav/player.py
+++ pylav/player.py
@@ -2074,7 +2074,7 @@ class Player(VoiceProtocol):
             tremolo=self.tremolo or None,
             vibrato=self.vibrato or None,
             distortion=self.distortion or None,
-            timescale=Timescale(speed=1.0, pitch=1.1, rate=1.1),
+            timescale=Timescale(speed=1.0, pitch=1.2, rate=1.2),
             channel_mix=self.channel_mix or None,
             echo=self.echo or None,
             reset_not_set=True,
