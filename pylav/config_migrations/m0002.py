afd17cb6efab84c6baeafd49c4313ba13f8e50c3
Draper <27962761+Drapersniper@users.noreply.github.com>
2022-11-29 19:53:28 +0000

    Bypass migration if starting version is `0.0.0`
    Signed-off-by: Draper <27962761+Drapersniper@users.noreply.github.com>

--- pylav/config_migrations/m0002.py
+++ pylav/config_migrations/m0002.py
@@ -3,12 +3,14 @@ from typing import TYPE_CHECKING
 from packaging.version import Version
 from packaging.version import parse as parse_version
 
+from pylav.constants import VERSION_ZERO
+
 if TYPE_CHECKING:
     from pylav.client import Client
 
 
 async def run_migration_0002(client: "Client", current_version: Version) -> None:
-    if current_version >= parse_version("0.0.0.2"):
+    if current_version >= parse_version("0.0.0.2") or current_version == VERSION_ZERO:
         return
     from pylav.config_migrations import LOGGER
 
--- pylav/config_migrations/m0850.py
+++ pylav/config_migrations/m0850.py
@@ -4,15 +4,18 @@ from typing import TYPE_CHECKING
 from packaging.version import Version
 from packaging.version import parse as parse_version
 
+from pylav.constants import VERSION_ZERO
+
 if TYPE_CHECKING:
     from pylav.client import Client
 
 
 async def run_migration_0850(client: "Client", current_version: Version) -> None:
-    if current_version >= parse_version("0.8.5"):
+    if current_version >= parse_version("0.8.5") or current_version == VERSION_ZERO:
         return
 
     from pylav.config_migrations import LOGGER
+    from pylav.constants import BUNDLED_PYLAV_PLAYLISTS_IDS
 
     LOGGER.info("Running 0.8.5.0 migration")
     playlists = [p for p in await client.playlist_db_manager.get_bundled_playlists() if p.id in {1, 2}]
