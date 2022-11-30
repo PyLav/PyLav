979628e71a19627619e9fb92ee7486640857046d
Draper <27962761+Drapersniper@users.noreply.github.com>
2022-11-30 20:19:01 +0000

    Updated base plugin versions
    Signed-off-by: Draper <27962761+Drapersniper@users.noreply.github.com>

--- pylav/utils/built_in_node.py
+++ pylav/utils/built_in_node.py
@@ -9,14 +9,14 @@ NODE_DEFAULT_SETTINGS = {
     "lavalink": {
         "plugins": [
             {
-                "dependency": "com.github.TopiSenpai.LavaSrc:lavasrc-plugin:3.0.6",
+                "dependency": "com.github.TopiSenpai.LavaSrc:lavasrc-plugin:3.1.6",
                 "repository": "https://jitpack.io",
             },
             {
-                "dependency": "com.dunctebot:skybot-lavalink-plugin:1.4.0",
+                "dependency": "com.dunctebot:skybot-lavalink-plugin:1.4.1",
                 "repository": "https://m2.duncte123.dev/releases",
             },
-            {"dependency": "com.github.topisenpai:sponsorblock-plugin:v1.0.3", "repository": "https://jitpack.io"},
+            {"dependency": "com.github.topisenpai:sponsorblock-plugin:1.0.4", "repository": "https://jitpack.io"},
             {"dependency": "me.rohank05:lavalink-filter-plugin:0.0.1", "repository": "https://jitpack.io"},
             {"dependency": "com.github.esmBot:lava-xm-plugin:v0.2.1", "repository": "https://jitpack.io"},
         ],
