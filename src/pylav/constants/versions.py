b68ce0d66981e0cbc71c3f325f15792ea03712a7
Draper <27962761+Drapersniper@users.noreply.github.com>
2022-12-03 13:34:10 +0000

    Contants

---
+++ src/pylav/constants/versions.py
@@ -0,0 +1,7 @@
+from packaging.version import parse
+
+VERSION_ZERO = parse("0.0.0")
+VERSION_3_6_0 = parse("3.6.0")
+VERSION_3_7_0 = parse("3.7.0")
+VERSION_4_0_0 = parse("4.0.0")
+VERSION_5_0_0 = parse("5.0.0")
