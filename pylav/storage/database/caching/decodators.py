ef968620ab01456f6351bbbae88e812c449fbb38
Draper <27962761+Drapersniper@users.noreply.github.com>
2022-12-09 18:16:11 +0000

    fix an issue where the cache was not being disabled properly and where it was returning a bad type
    Signed-off-by: Draper <27962761+Drapersniper@users.noreply.github.com>

--- pylav/storage/database/caching/decodators.py
+++ pylav/storage/database/caching/decodators.py
@@ -4,6 +4,7 @@ import functools
 from collections.abc import Awaitable
 from typing import Callable
 
+from pylav.constants.config import READ_CACHING_ENABLED
 from pylav.storage.database.caching.cache import CACHE
 from pylav.storage.database.caching.functions import key_builder
 from pylav.type_hints.generics import ANY_GENERIC_TYPE, PARAM_SPEC_TYPE
@@ -14,6 +15,8 @@ def maybe_cached(func: Callable[[PARAM_SPEC_TYPE], ANY_GENERIC_TYPE]) -> Callabl
     async def wrapper(
         *args: PARAM_SPEC_TYPE.args, **kwargs: PARAM_SPEC_TYPE.kwargs
     ) -> Awaitable[Callable[[PARAM_SPEC_TYPE], ANY_GENERIC_TYPE]]:
-        return await CACHE(ttl=None, key=key_builder(func, *args, **kwargs))(func)(*args, **kwargs)
+        if READ_CACHING_ENABLED:
+            return await CACHE(ttl=None, key=key_builder(func, *args, **kwargs))(func)(*args, **kwargs)
+        return func(*args, **kwargs)
 
     return wrapper
