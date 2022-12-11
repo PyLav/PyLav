763947b94791f019975321a0a8eac06e26ccb85d
Draper <27962761+Drapersniper@users.noreply.github.com>
2022-12-11 13:07:58 +0000

    Use poetry as build system
    Signed-off-by: Draper <27962761+Drapersniper@users.noreply.github.com>

--- pylav/__init__.py
+++ pylav/__init__.py
@@ -5,7 +5,7 @@ import typing
 
 from packaging.version import Version, parse
 
-__version__: str = importlib.metadata.version("Py-Lav")
+__version__ = "0.0.0"
 VERSION: Version = typing.cast(Version, parse(__version__))
 __PATH = None if (__PATHS := importlib.metadata.files("Py-Lav")) is None else next(iter(__PATHS), None)
 LOCATION = __PATH.locate() if __PATH else None
