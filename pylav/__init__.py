2c943680ee411eb910b8a874c09f6b8e6d675081
Draper <27962761+Drapersniper@users.noreply.github.com>
2022-12-30 18:18:52 +0000

    update classifiers, tool deps and config
    Signed-off-by: Draper <27962761+Drapersniper@users.noreply.github.com>

--- pylav/__init__.py
+++ pylav/__init__.py
@@ -1,14 +1,14 @@
 from __future__ import annotations
 
-import importlib.metadata
 import typing
 
+import importlib_metadata
 from packaging.version import Version, parse
 
 from pylav.__version__ import __version__ as __version__
 
 VERSION: Version = typing.cast(Version, parse(__version__))
-__PATH = None if (__PATHS := importlib.metadata.files("Py-Lav")) is None else next(iter(__PATHS), None)
+__PATH = None if (__PATHS := importlib_metadata.files("Py-Lav")) is None else next(iter(__PATHS), None)
 LOCATION = __PATH.locate() if __PATH else None
 
 
