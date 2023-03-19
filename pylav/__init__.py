dcf8cad49b8eea0279fbae936288b11292414698
Draper <27962761+Drapersniper@users.noreply.github.com>
2023-03-19 20:31:01 +0000

    Fix an error which would be raised if the Package is not installed

--- pylav/__init__.py
+++ pylav/__init__.py
@@ -8,7 +8,10 @@ from packaging.version import Version, parse
 from pylav.__version__ import __version__ as __version__
 
 VERSION: Version = typing.cast(Version, parse(__version__))
-__PATH = None if (__PATHS := importlib_metadata.files("Py-Lav")) is None else next(iter(__PATHS), None)
+try:
+    __PATH = None if (__PATHS := importlib_metadata.files("Py-Lav")) is None else next(iter(__PATHS), None)
+except importlib_metadata.PackageNotFoundError:
+    __PATH = None
 LOCATION = __PATH.locate() if __PATH else None
 
 
