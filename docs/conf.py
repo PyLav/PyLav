c3ce2f469a664e361523548000d3817627ad4d81
Draper <27962761+Drapersniper@users.noreply.github.com>
2023-03-19 20:18:54 +0000

    Fix import

--- docs/conf.py
+++ docs/conf.py
@@ -11,7 +11,7 @@ sys.path.insert(0, os.path.abspath(".."))
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-from ..pylav import __version__
+from pylav import __version__
 
 project = "PyLav"
 copyright = "2023, Drapersniper"
