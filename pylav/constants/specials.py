0fdcca09461b4a240cf47534c18f740f5d7638b5
Draper <27962761+Drapersniper@users.noreply.github.com>
2023-05-08 10:03:30 +0100

    Fix issues

--- pylav/constants/specials.py
+++ pylav/constants/specials.py
@@ -1,7 +1,5 @@
 from __future__ import annotations
 
-from pylav._internals.functions import fix
-
 _MAPPING = {
     2: ([0, 2, 5], 5),
     1: ([0, 1, 4], 5),
@@ -9,13 +7,5 @@ _MAPPING = {
     0: ([0, 1, 4, 5], 5),
 }
 
-
-__ZERO__ = fix("Al0YW" "MHp2Zg" "-1d5G", _MAPPING[2], e=True)
-# noinspection SpellCheckingInspection
-__MOMONGA__ = fix("ZiL4Z" "NTh3Yw" "Y8prT", _MAPPING[1], e=True)
-__LIGHT__ = fix("Id58Z" "OW5hMQ" "Hdv24", _MAPPING[0], e=True)
-__SEPHIROTH__ = fix("9-Diz" "ZzRlbA" "8Q4H2", _MAPPING[2], e=True)
-
-
-ANIME = b"|".join([__SEPHIROTH__, __MOMONGA__, __ZERO__, __LIGHT__])
+ANIME = "id58zow5hmqhdv24zil4znth3ywy8prt9-dizzzrlba8q4h2al0ywmhp2zg-1d5g"
 __all__ = ("ANIME",)
