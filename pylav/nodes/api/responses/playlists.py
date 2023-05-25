0d880ab2c1f887562bb241c4c55404e632c87ca2
Draper <27962761+Drapersniper@users.noreply.github.com>
2023-05-25 16:15:37 +0100

    Implement https://github.com/Walkyst/lavaplayer-fork/compare/custom...TopiSenpai:lavaplayer-fork:extend-audioplaylist-interface with backwards compat

--- pylav/nodes/api/responses/playlists.py
+++ pylav/nodes/api/responses/playlists.py
@@ -1,9 +1,14 @@
 from __future__ import annotations
 
 import dataclasses
+from typing import NotRequired
 
 
 @dataclasses.dataclass(repr=True, frozen=True, kw_only=True, slots=True)
 class Info:
     name: str
     selectedTrack: int
+    type: NotRequired[str] = "playlist"
+    identifier: NotRequired[str | None] = None
+    artworkUrl: NotRequired[str | None] = None
+    author: NotRequired[str | None] = None
